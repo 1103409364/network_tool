09459a902ec0bbbd1014792cb15ec90f52af38e7
15x <1103409364@email.com>
2025-01-19 16:45:01 +0800

    docs: Enhance comments in main.rs for better clarity on module imports and functionality
    - Improved comments for module imports to clarify their purpose.
    - Added explanations for the main function's operations, including single instance enforcement and server/client startup processes.
    
    Signed-off-by: 15x <1103409364@email.com>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #![windows_subsystem = "windows"] // 在 Windows 上禁用控制台窗口
-use log::error;
-use single_instance::SingleInstance;
+use log::error; // 使用 log 模块的 error 宏
+use single_instance::SingleInstance; // 单实例库
 mod client; // 导入 mod client 模块。
-mod common;
+mod common; // 导入 mod common 模块。
 mod server; // 导入 mod server 模块。文件即模块，不需要额外声明 目录下有 mod.rs 文件也是模块
+
 #[cfg(test)]
-// 条件编译，只有在测试模式下才编译测试代码，只有当你运行 cargo test 命令时，这个模块才会被编译和执行
-mod tests; // 导入测试模块
+mod tests; // 导入测试模块 // 条件编译，只有在测试模式下才编译测试代码，只有当你运行 cargo test 命令时，这个模块才会被编译和执行
+
 fn main() {
     // 确保程序只运行一个实例
     let instance = SingleInstance::new("2eHYAHYbarsMt3f").unwrap();
@@ -14,8 +15,7 @@ fn main() {
         error!("The program is already running.");
         return;
     }
-    common::log::config();
-    // 启动 Web 服务器
-    server::main::run(); // use server::main::run;
+    common::log::config(); // 配置日志
+    server::main::run(); // // 启动 Web 服务器 use server::main::run;
     client::main::run(); // 启动客户端程序 事件循环阻塞主线程
 }
